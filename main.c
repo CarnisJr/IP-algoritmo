12adb1ed70567686a4535414b87c703be824687d
Christopher Miranda <mirandab@gmail.com>
2023-05-10 18:14:45 -0500

    Segundo commit

--- main.c
+++ main.c
@@ -7,20 +7,22 @@ int main(void){
     
     IP32bits = 0;
     
-    scanf("%d", &partIP1);
-    scanf("%d", &partIP2);
-    scanf("%d", &partIP3);
-    scanf("%d", &partIP4);
-    
+    printf("\t---LEER IP---\n");
+	printf("Ingresa la primera parte: ");
+	scanf("%d", &partIP1);
+	printf("Ingresa la segunda parte: ");
+	scanf("%d", &partIP2);
+	printf("Ingresa la tercera parte: ");
+	scanf("%d", &partIP3);
+	printf("Ingresa la cuarta parte: ");
+	scanf("%d", &partIP4);
+
     if(partIP1 >= 0 && partIP1 <= 255 &&
        partIP2 >= 0 && partIP2 <= 255 &&
        partIP3 >= 0 && partIP3 <= 255 &&
        partIP4 >= 0 && partIP4 <= 255){
            
-            IP32bits += (unsigned long) partIP1 * 256 * 256 * 256;
-            IP32bits += (unsigned long) partIP2 * 256 * 256;
-            IP32bits += (unsigned long) partIP3 * 256;
-            IP32bits += (unsigned long) partIP4;
+            IP32bits += (long) ((partIP1 * 256 * 256 * 256) + (partIP2 * 256 * 256) + (partIP3 * 256) + (partIP4));
             
             printf("Su IP en IPv4 es: %d.%d.%d.%d\n", partIP1, partIP2, partIP3, partIP4);
             printf("Su IP en 32bits es: %lu\n", IP32bits);
